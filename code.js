5b16286d6134385741f3f38584934b8118456142
wjtam <wayne0tam@gmail.com>
2019-05-07 03:18:29 -0400

    use bandwidth() instead of barWidth variable

--- code.js
+++ code.js
@@ -11,19 +11,29 @@ $(document).ready(function () {
         width = +svg.attr("width") - margin.left - margin.right,
         height = +svg.attr("height") - margin.top - margin.bottom,
         g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    var barWidth = width/24;
+    
+    //bandwidth() seems to do the same thing here
+    //var barWidth = width/24;
 
     var x = d3.scaleBand()
-        .domain(["January", "February", "March", "April", "May", "June", "July", "August", "September", 
-                    "October", "November", "December"])
+        // .domain(["January", "February", "March", "April", "May", "June", "July", "August", "September", 
+        //              "October", "November", "December"])
         .rangeRound([0, width])
-        .padding(0.5);
+        .padding(0.3);
 
     var y = d3.scaleLinear()
         .rangeRound([height, 0]);
 
+    /*
+    * Bar Graph Drawer 
+    * Function can be mad modular by renaming data to [{"x":"xData", "y":"yData"}]
+    * This will chang eit so we don't need to use d.Month but d.x instead
+    */
     function draw(data) {
+        x.domain(data.map(function (d) {
+            return d.Month;
+        }));
+
         y.domain([0, d3.max(data, function (d) {
             return Number(d.Freq);
         })]);
@@ -47,7 +57,7 @@ $(document).ready(function () {
             .enter().append("rect")
             .attr("class", "bar")
             .attr("x", function (d) { return x(d.Month); })
-            .attr("width", barWidth)
+            .attr("width", x.bandwidth())
             .attr("y", function (d) { return y(d.Freq); })
             .attr("height", function (d) { return height - y(d.Freq); });
     }
