7b7333946eec330460850c6cea28aec197d61a13
charithaa15 <charithaa15@gmail.com>
2019-05-07 22:43:44 -0400

    Update PerpSex.js

--- datasets/PerpSex.js
+++ datasets/PerpSex.js
@@ -1,1 +1,1 @@
-[{"Perpetrator_Sex": "Male", "Freq": 399541}, {"Perpetrator_Sex": "Unknown", "Freq": 190365}, {"Perpetrator_Sex": "Female", "Freq": 48548}]
+var perpSex = [{"Perpetrator_Sex": "Male", "Freq": 399541}, {"Perpetrator_Sex": "Unknown", "Freq": 190365}, {"Perpetrator_Sex": "Female", "Freq": 48548}]
