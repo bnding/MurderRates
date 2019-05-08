0653c0b3600bf9be71cd108017f4b870444225bb
charithaa15 <charithaa15@gmail.com>
2019-05-07 22:45:38 -0400

    Update VictSex.js

--- datasets/VictSex.js
+++ datasets/VictSex.js
@@ -1,1 +1,1 @@
-[{"Victim_Sex": "Male", "Freq": 494125}, {"Victim_Sex": "Female", "Freq": 143345}, {"Victim_Sex": "Unknown", "Freq": 984}]
+var victSex = [{"Victim_Sex": "Male", "Freq": 494125}, {"Victim_Sex": "Female", "Freq": 143345}, {"Victim_Sex": "Unknown", "Freq": 984}]
